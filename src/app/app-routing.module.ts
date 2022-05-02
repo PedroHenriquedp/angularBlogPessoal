59d1e2de776900e1d268838a37e1e7732217cf60
PedroHenriquedp <pereira.henrique@aluno.ufabc.edu.br>
2022-05-02 04:27:43 -0300

    CRIANDO ROTAS 🚪

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+// NECESSÁRIO CRIAR A LINHA SEGUINTE PARA COMEÇAR NA PAGINA LOGIN E AI DEPOIS IR PARA REGISTER!!!
+  {path:'', redirectTo:'login',pathMatch:'full'},
+
+
+  {path:'login',component:LoginComponent},
+  {path:'register', component:RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
